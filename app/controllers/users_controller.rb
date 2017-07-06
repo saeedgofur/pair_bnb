7b4160a92358d0c4dea8f6757dcd3dddf9a0c199
Toothless3010 <p.agilan93@gmail.com>
2017-07-06 12:19:08 +0800

    Add the Clearance gem. Install the gem into our app. Generate Clearance routes for customization. Generate Clearance views for frontend customization

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < Clearance::UsersController
+  
+  def create
+     @user = user_from_params
+
+     if @user.save
+       sign_in @user
+       redirect_to welcome_path
+     else
+       render template: "users/new"
+     end
+ end
+
+end
